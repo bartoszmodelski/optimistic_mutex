b9cfb2451453a6e362b91ac38d96bfb0d52305f4
Bartosz Modelski <modelski.bartosz@gmail.com>
2023-06-07 11:48:22 +0100

    simplify mcs

--- lib/mcs_spinlock.ml
+++ lib/mcs_spinlock.ml
@@ -1,37 +1,38 @@
 type node = { locked : bool ref; next : next Atomic.t }
 and next = Node of node | Nil
 
-type t = next Atomic.t
+type t = { head : next Atomic.t Atomic.t; tail : next ref }
 
 let nil = Nil
 
 let create () =
-  Atomic.make (Node { locked = ref false; next = Atomic.make nil })
+  let next = Atomic.make nil in
+  let head = Atomic.make next in
+  let tail = ref (Node ({ locked = ref false; next } : node)) in
+  { head; tail }
 
-let lock (t : t) =
+let lock ({ tail; _ } : t) =
   let new_node = Node ({ locked = ref true; next = Atomic.make nil } : node) in
-  let rec traverse_and_insert (({ locked; next } : node) as current) =
-    match Atomic.get next with
-    | Node actual_next -> traverse_and_insert actual_next
-    | Nil ->
-        if not (Atomic.compare_and_set next nil new_node) then
-          traverse_and_insert current
-        else
-          while !locked do
-            Domain.cpu_relax ()
-          done
-  in
-  match Atomic.get t with
+  match !tail with
   | Nil -> assert false
-  | Node hd -> traverse_and_insert hd
+  | Node node ->
+      let rec traverse_and_insert (({ locked; next } : node) as current) =
+        match Atomic.get next with
+        | Node actual_next -> traverse_and_insert actual_next
+        | Nil ->
+            if not (Atomic.compare_and_set next nil new_node) then
+              traverse_and_insert current
+            else (
+              tail := new_node;
+              while !locked do
+                Domain.cpu_relax ()
+              done)
+      in
+      traverse_and_insert node
 
-let unlock (t : t) =
-  match Atomic.get t with
+let unlock ({ head; _ } : t) =
+  match Atomic.get (Atomic.get head) with
   | Nil -> assert false
-  | Node { next; _ } -> (
-      let next_val = Atomic.get next in
-      Atomic.set t next_val;
-
-      match next_val with
-      | Nil -> assert false
-      | Node { locked; _ } -> locked := false)
+  | Node { next; locked } ->
+      Atomic.set head next;
+      locked := false
